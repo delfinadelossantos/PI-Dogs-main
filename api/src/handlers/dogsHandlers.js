f0dd382c6ddfc906d24a9cf32e65e9cde01ae671
delfinadelossantos <delfinadelossantos@gmail.com>
2023-06-04 11:40:21 -0300

    created handlers and models

---
+++ api/src/handlers/dogsHandlers.js
@@ -0,0 +1,27 @@
+const getDogsHandler = (req, res) => {
+  //llama a la función que obtiene los datos de la BDD
+  //llama a la función que obtiene los datos de la API externa
+  //une los datos unificando el formato de ambas
+  //responde con todos los datos.
+  const { breed } = req.query;
+  if (breed) {
+    res.status(200).send(`Send all dogs with breed ${breed}`);
+  } else {
+    res.status(200).send("Send all dogs");
+  }
+};
+
+const getDogHandler = (req, res) => {
+  try {
+    const { id } = req.params;
+    res.status(200).send(`Send detail about dog with id ${id}`);
+  } catch (error) {}
+};
+
+const createDogHandler = (req, res) => {};
+
+module.exports = {
+  getDogHandler,
+  getDogsHandler,
+  createDogHandler,
+};
--- api/src/routes/dogsRouter.js
+++ api/src/routes/dogsRouter.js
@@ -2,26 +2,33 @@ const { Router } = require("express");
 
 const dogsRouter = Router();
 
+const {
+  getDogsHandler,
+  getDogHandler,
+  createDogHandler,
+} = require("../handlers/dogsHandlers");
+
 // GET | /dogs
 // Obtiene un arreglo de objetos, donde cada objeto es la raza de un perro.
-dogsRouter.get();
+// INCLUYE:
+// GET | /dogs/name?="..."
+// Esta ruta debe obtener todas aquellas razas de perros que coinciden con el nombre recibido por query. (No es necesario que sea una coincidencia exacta).
+// Debe poder buscarlo independientemente de mayúsculas o minúsculas.
+// Si no existe la raza, debe mostrar un mensaje adecuado.
+// Debe buscar tanto los de la API como los de la base de datos.
+dogsRouter.get("/", getDogsHandler);
 
 // GET | /dogs/:idRaza
 // Esta ruta obtiene el detalle de una raza específica. Es decir que devuelve un objeto con la información pedida en el detalle de un perro.
 // La raza es recibida por parámetro (ID).
 // Tiene que incluir los datos de los temperamentos asociadas a esta raza.
 // Debe funcionar tanto para los perros de la API como para los de la base de datos.
-
-// GET | /dogs/name?="..."
-// Esta ruta debe obtener todas aquellas razas de perros que coinciden con el nombre recibido por query. (No es necesario que sea una coincidencia exacta).
-// Debe poder buscarlo independientemente de mayúsculas o minúsculas.
-// Si no existe la raza, debe mostrar un mensaje adecuado.
-// Debe buscar tanto los de la API como los de la base de datos.
+dogsRouter.get("/:id", getDogHandler);
 
 // POST | /dogs
 // Esta ruta recibirá todos los datos necesarios para crear un nuevo perro y relacionarlo con los temperamentos asociados.
 // Toda la información debe ser recibida por body.
 // Debe crear la raza de perro en la base de datos, y esta debe estar relacionada con los temperamentos indicados (al menos uno).
-dogsRouter.post();
+dogsRouter.post("/", createDogHandler);
 
 module.exports = dogsRouter;
