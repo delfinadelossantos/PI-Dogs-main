0fcf447c7ec00f01924b6bc69718910b9bfdc1f0
delfinadelossantos <delfinadelossantos@gmail.com>
2023-06-14 19:56:03 -0300

    NavBar and useLocation

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
-import { Route } from "react-router-dom";
+import { Route, useLocation } from "react-router-dom";
 import { Detail, Form, Home, Landing } from "./views";
 import NavBar from "./components/NavBar/NavBar";
 
 function App() {
+  //useLocation es un hook que permite acceder a la ubicación actual (path)
+
+  const location = useLocation();
+
   return (
     <div className="App">
-      <NavBar></NavBar>
+      {/* Lo utilizo para que el navbar sólo se muestre en los paths requeridos. */}
+      {location.pathname !== "/" && <NavBar />}
 
       <Route exact path="/">
         <Landing />
