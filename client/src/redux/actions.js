91a9557e4e8d12f07b88d4b81959c8e8dc22ac5a
delfinadelossantos <delfinadelossantos@gmail.com>
2023-06-16 01:36:31 -0300

    actions and reducer

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,19 +1,41 @@
 import axios from "axios";
 export const GET_DOGS = "GET_DOGS";
+export const SEARCH_DOG = "SEARCH_DOG";
+export const GET_DOG_DETAIL = "GET_DOG_DETAIL";
+export const FILTER_BY_SOURCE = "FILTER_BY_SOURCE";
 
 export const getDogs = () => {
   return async function (dispatch) {
     const backendDogs = await axios.get("http://localhost:3001/dogs/");
     const dogs = backendDogs.data;
-    dispatch({ type: GET_DOGS, payload: dogs });
+    dispatch({ type: "GET_DOGS", payload: dogs });
   };
 };
 
-//SEARCH_DOG
+export const searchDog = (name) => {
+  return async function (dispatch) {
+    const search = await axios.get(
+      "http://localhost:3001/dogs/search?name=" + name
+    );
+    dispatch({ type: SEARCH_DOG, payload: search.data });
+  };
+};
+
+export const getDogDetail = (id) => {
+  return async function (dispatch) {
+    const detail = await axios.get(`http://localhost:3001/dogs/${id}`);
+    dispatch({ type: GET_DOG_DETAIL, payload: detail.data });
+  };
+};
+
+export const filterBySource = () => {
+  return function (dispatch) {
+    dispatch({ type: FILTER_BY_SOURCE });
+  };
+};
+
 //FILTER_TEMPERAMENT
-//FILTER_ORIGIN
 //SORT_BREEDS
 //SORT_WEIGHT
 //RESET_FILTERS
-//GET_DOG_DETAIL
 //CREATE_BREED
--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,6 +1,18 @@
 import CardsContainer from "../../components/CardsContainer/CardsContainer";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+//Importo la action que debe despacharse:
+import { getDogs } from "../../redux/actions";
 
 const Home = () => {
+  const dispatch = useDispatch();
+
+  //useEffect recibe una función que se ejecuta cuando el componente se
+  //monta o cuando cambia el array de dependencias.
+  useEffect(() => {
+    dispatch(getDogs());
+  }, [dispatch]);
+
   return (
     <>
       <h1>Home</h1>
