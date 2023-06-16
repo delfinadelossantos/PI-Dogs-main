b449bbaf57127c883ce8e3709a97b49c8afa5877
delfinadelossantos <delfinadelossantos@gmail.com>
2023-06-16 01:36:23 -0300

    actions and reducer

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,7 +1,7 @@
 //El reducer es una función que recibe el estado a modificar y la action
 //que indica qué tiene que hacer.
 
-import { GET_DOGS } from "./actions";
+import { GET_DOGS, SEARCH_DOG, GET_DOG_DETAIL } from "./actions";
 
 //El estado global al principio de la aplicación es el initialState
 const initialState = {
@@ -15,8 +15,11 @@ const rootReducer = (state = initialState, action) => {
   //copia del estado.
   switch (action.type) {
     case GET_DOGS:
-      return { ...state, users: action.payload };
-
+      return { ...state, breeds: action.payload };
+    // case SEARCH_DOG:
+    //   return { ...state, breeds: action.payload };
+    // case GET_DOG_DETAIL:
+    //   return { ...state, detail: action.payload };
     default:
       return { ...state };
   }
