e92baef23041c2c072948aa1ac11a2ce1806ab12
delfinadelossantos <delfinadelossantos@gmail.com>
2023-06-17 19:33:03 -0300

    created Landing

--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
+import style from "./Landing.module.css";
+
 const Landing = () => {
   return (
-    <>
-      <h1>Welcome to Henry Dogs</h1>
-    </>
+    <div className={style.Landing}>
+      <h1 className={style.title}>Welcome to Henry Dogs!</h1>
+      <Link to="/home">
+        <button className={style.accessbtn}>Home</button>
+      </Link>
+    </div>
   );
 };
 
