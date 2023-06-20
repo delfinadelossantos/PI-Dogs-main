edf99c0f555e1849f693137890a86370cb60d7ae
delfinadelossantos <delfinadelossantos@gmail.com>
2023-06-20 10:02:41 -0300

    added Form validations

--- client/src/views/Form/Form.jsx
+++ client/src/views/Form/Form.jsx
@@ -1,7 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
+import { getTemperaments } from "../../redux/actions";
+import { useSelector, useDispatch } from "react-redux";
 
 const Form = () => {
+  const dispatch = useDispatch();
+
   const [form, setForm] = useState({
     breed: "",
     min_height: 0,
@@ -24,6 +28,16 @@ const Form = () => {
     image: "",
   });
 
+  //Control del estado del formulario para que no pueda
+  //submitearse sin estar validado.
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  useEffect(() => {
+    dispatch(getTemperaments());
+  }, [dispatch]);
+
+  const temperaments = useSelector((state) => state.temperaments);
+
   //El input debe ser el reflejo del estado. Al cambiar cualquier
   //dato en el input, es necesario que se ejecute una función que cambie
   //el estado, para que los cambios se vean reflejados.
@@ -43,17 +57,125 @@ const Form = () => {
 
   //Función que valida si al hacer un cambio, lo que se ingresa al input es correcto
   const validate = (form) => {
-    //Acá hago validaciones con if.
-    //el error se guarda en setErrors y se muestra
-    //ejemplo:
-    //   if(regex.test(form.algo)){
-    //     setErrors({...errors, algo:""}){
-    //       else{
-    //         setErrors({...errors, algo:"hay un error en el nombre"})
-    //       }
-    //     }
-    //   }
-    //   if(form.algo==="") setErrors({errors, algo: "campo vacío"})
+    let formValid = true;
+    const regexBreed = /^[A-Za-z]+$/; // Expresión regular para validar palabra sin números ni caracteres especiales
+    const regexHeight = /^\d+$/; // Expresión regular para validar números enteros
+    const regexWeight = /^\d+$/; // Expresión regular para validar números enteros
+
+    if (!form.breed) {
+      setErrors({ ...errors, breed: "Breed field cannot be empty" });
+    } else if (!regexBreed.test(form.breed)) {
+      setErrors({
+        ...errors,
+        breed: "Only letters are allowed in the Breed field",
+      });
+    } else {
+      setErrors({ ...errors, breed: "" });
+    }
+
+    if (!form.min_height) {
+      setErrors({
+        ...errors,
+        min_height: "Minimum Height field cannot be empty",
+      });
+    } else if (!regexHeight.test(form.min_height)) {
+      setErrors({ ...errors, min_height: "Minimum Height must be a number" });
+    } else if (parseInt(form.min_height) <= 0) {
+      setErrors({
+        ...errors,
+        min_height: "Minimum Height must be greater than zero",
+      });
+    } else {
+      setErrors({ ...errors, min_height: "" });
+    }
+
+    if (!form.max_height) {
+      setErrors({
+        ...errors,
+        max_height: "Maximum Height field cannot be empty",
+      });
+    } else if (!regexHeight.test(form.max_height)) {
+      setErrors({ ...errors, max_height: "Maximum Height must be a number" });
+    } else if (parseInt(form.max_height) <= 0) {
+      setErrors({
+        ...errors,
+        max_height: "Maximum Height must be greater than zero",
+      });
+    } else if (parseInt(form.max_height) <= parseInt(form.min_height)) {
+      setErrors({
+        ...errors,
+        max_height: "Maximum Height must be greater than Minimum Height",
+      });
+    } else {
+      setErrors({ ...errors, max_height: "" });
+    }
+
+    if (!form.min_weight) {
+      setErrors({
+        ...errors,
+        min_weight: "Minimum Weight field cannot be empty",
+      });
+    } else if (!regexWeight.test(form.min_weight)) {
+      setErrors({ ...errors, min_weight: "Minimum Weight must be a number" });
+    } else if (parseInt(form.min_weight) <= 0) {
+      setErrors({
+        ...errors,
+        min_weight: "Minimum Weight must be greater than zero",
+      });
+    } else {
+      setErrors({ ...errors, min_weight: "" });
+    }
+
+    if (!form.max_weight) {
+      setErrors({
+        ...errors,
+        max_weight: "Maximum Weight field cannot be empty",
+      });
+    } else if (!regexWeight.test(form.max_weight)) {
+      setErrors({ ...errors, max_weight: "Maximum Weight must be a number" });
+    } else if (parseInt(form.max_weight) <= 0) {
+      setErrors({
+        ...errors,
+        max_weight: "Maximum Weight must be greater than zero",
+      });
+    } else if (parseInt(form.max_weight) <= parseInt(form.min_weight)) {
+      setErrors({
+        ...errors,
+        max_weight: "Maximum Weight must be greater than Minimum Weight",
+      });
+    } else {
+      setErrors({ ...errors, max_weight: "" });
+    }
+
+    if (!form.life_span) {
+      setErrors({ ...errors, life_span: "Life Span field cannot be empty" });
+    } else if (!regexHeight.test(form.life_span)) {
+      setErrors({ ...errors, life_span: "Life Span must be a number" });
+    } else {
+      setErrors({ ...errors, life_span: "" });
+    }
+
+    if (!form.temperaments || form.temperaments.length === 0) {
+      setErrors({
+        ...errors,
+        temperaments: "At least one temperament must be chosen",
+      });
+    } else {
+      setErrors({ ...errors, temperaments: "" });
+    }
+
+    if (
+      errors.breed ||
+      errors.min_height ||
+      errors.max_height ||
+      errors.min_weight ||
+      errors.max_weight ||
+      errors.life_span ||
+      errors.temperaments
+    ) {
+      formValid = false;
+    }
+    setIsFormValid(formValid);
   };
 
   const submitHandler = (event) => {
@@ -75,9 +197,9 @@ const Form = () => {
           name="breed"
         />
       </div>
-      {/* Acá iría un span (chequear si corresponde ese tag) informando el {errors.algo} del validate */}
+      <span>{errors.breed}</span>
       <div>
-        <label>Minimum Height: </label>
+        <label>Minimum Height in cm: </label>
         <input
           type="text"
           value={form.min_height}
@@ -85,9 +207,9 @@ const Form = () => {
           name="min_height"
         />
       </div>
-
+      <span>{errors.min_height}</span>
       <div>
-        <label>Maximum Height: </label>
+        <label>Maximum Height in cm: </label>
         <input
           type="text"
           value={form.max_height}
@@ -95,9 +217,9 @@ const Form = () => {
           name="max_height"
         />
       </div>
-
+      <span>{errors.max_height}</span>
       <div>
-        <label>Minimum Weight: </label>
+        <label>Minimum Weight in kg: </label>
         <input
           type="text"
           value={form.min_weight}
@@ -105,9 +227,9 @@ const Form = () => {
           name="min_weight"
         />
       </div>
-
+      <span>{errors.min_weight}</span>
       <div>
-        <label>Maximum Weight: </label>
+        <label>Maximum Weight in kg: </label>
         <input
           type="text"
           value={form.max_weight}
@@ -115,7 +237,7 @@ const Form = () => {
           name="max_weight"
         />
       </div>
-
+      <span>{errors.max_weight}</span>
       <div>
         <label>Lifespan: </label>
         <input
@@ -125,15 +247,26 @@ const Form = () => {
           name="life_span"
         />
       </div>
+      <span>{errors.life_span}</span>
       {/* Temperamentos */}
       <div>
         <label>Temperaments: </label>
-        <input
-          type="text"
+        <select
+          name="temperaments"
           value={form.temperaments}
           onChange={changeHandler}
-          name="temperaments"
-        />
+          multiple>
+          Select Temperaments
+          <option value="All">All Temperaments</option>
+          {temperaments.map((temperament) => (
+            <option key={temperament.id} value={temperament.temperament}>
+              {temperament.temperament}
+            </option>
+          ))}
+        </select>
+        <span>{errors.temperaments}</span>
+        <br />
+        {/* <span>Temperaments selected: {form.temperaments.join(", ")}</span> */}
       </div>
       {/* Imagen */}
       <div>
@@ -145,16 +278,11 @@ const Form = () => {
           name="image"
         />
       </div>
-      <button type="submit">SUBMIT</button>
+      <button type="submit" disabled={!isFormValid}>
+        SUBMIT
+      </button>
     </form>
   );
 };
 
 export default Form;
-
-// Posibilidad de seleccionar/agregar varios temperamentos en simultáneo.
-// Botón para crear la nueva raza.
-// [IMPORANTE]: es requisito que el formulario de creación esté validado
-// sólo con JavaScript. Puedes agregar las validaciones que consideres.
-// Por ejemplo: que el nombre de la raza no pueda contener números, o
-// que el peso/altura mínimo no pueda ser mayor al máximo.
